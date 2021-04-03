426966eb75c6d2da8378f97f0e724ace0c51b8b9
Kevin Apodaca <kevin.apodaca7@gmail.com>
2021-04-03 16:56:48 -0600

    simple 404 page

---
+++ layouts/error.vue
@@ -0,0 +1,24 @@
+<template>
+  <div style="text-align: center;">
+    <img src="https://media1.tenor.com/images/99f00b32545bf5b5db8bf8ecbb7f0aec/tenor.gif?itemid=7971019"/><br/><br/>
+    <h1>Error! 🙀</h1>
+    {{ error.message }} {{ error.statusCode }}
+    </div>
+</template>
+
+<script>
+export default {
+  head() {
+    return {
+      title: 'Uh Oh, Something is Wrong 🙀'
+    }
+  },
+  props: {
+    error: {
+      type: Object,
+      required: true
+    }
+  }
+
+}
+</script>
