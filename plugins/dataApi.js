722e928e6e06f2811ac7880cd8165aef9cb31bce
Kevin Apodaca <kevin.apodaca7@gmail.com>
2021-04-03 16:46:40 -0600

    add functions to handle error messages

--- plugins/dataApi.js
+++ plugins/dataApi.js
@@ -1,18 +1,38 @@
 export default function(context,inject) {
   const appId = '7C3A29AHIT'
   const apiKey = '311f68154c006577fc0044311ee4a288'
+  const headers = {
+    'X-Algolia-API-Key': apiKey,
+    'X-Algolia-Application-Id': appId,
+  }
 
   inject('dataApi', {
     getHome
   })
   async function getHome(homeId) {
-    const response = await fetch(`https://${appId}-dsn.algolia.net/1/indexes/homes/${homeId}`, {
-      headers: {
-        'X-Algolia-API-Key': apiKey,
-        'X-Algolia-Application-Id': appId,
-      }
-    })
+    try {
+      return unWrap(await fetch(`https://${appId}-dsn.algolia.net/1/indexes/homes/${homeId}`, { headers }))
+    } catch (error) {
+      return getErrorResponse(error)
+    }
+  }
+
+  async function unWrap(response) {
     const json = await response.json()
-    return json
+    const { ok, status, statusText } = response
+    return {
+      json,
+      ok,
+      status,
+      statusText
+    }
+  }
+  function getErrorResponse(error) {
+    return {
+      ok: false,
+      status: 500,
+      statusText: error.message,
+      json: {}
+    }
   }
 }
