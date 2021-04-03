1f3129f966586ec23c72fbb232c9dd07e6c2a8f4
Kevin Apodaca <kevin.apodaca7@gmail.com>
2021-04-03 17:39:23 -0600

    :sparkles: read more read less functionality

--- pages/home/_id.vue
+++ pages/home/_id.vue
@@ -15,7 +15,7 @@
       <img :src="review.reviewer.image"/><br/>
       {{ review.reviewer.name }}<br/>
       {{ formatDate(review.date) }}<br/>
-      {{ review.comment }}<br/>
+      <short-text :text="review.comment" :target="150"/><br/>
     </div>
 
   </div>
