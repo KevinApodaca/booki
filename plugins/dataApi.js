2fd63d3843399ebc55734896e1f88209e9b4f077
Kevin Apodaca <kevin.apodaca7@gmail.com>
2021-04-03 17:38:45 -0600

    :building_construction: cleanup api response

--- plugins/dataApi.js
+++ plugins/dataApi.js
@@ -25,6 +25,8 @@ export default function(context,inject) {
         method: 'POST',
         body: JSON.stringify({
           filters: `homeId:${homeId}`,
+          hitsPerPage: 6, /* only this many reviews per page */
+          attributesToHighlight: []
         })
       }))
     } catch(error) {
