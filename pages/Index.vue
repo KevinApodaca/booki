32260f5d048def117d16390cbe36a4a2587e7004
Kevin Apodaca <kevin.apodaca7@gmail.com>
2021-04-03 12:18:14 -0600

    :recycle: abstract homes into card components

--- pages/Index.vue
+++ pages/Index.vue
@@ -1,19 +1,18 @@
 <template>
   <div>
-  <div v-for="home in homes" :key="home.objectID" style="float:left; margin:10px">
-    <img :src="home.images[0]" style="width:200px"><br/>
-    {{ home.title }}<br/>
-    {{ home.location.address }} {{ home.location.city }} {{ home.location.state }}<br/>
-    {{ home.guests }} guests<br/>
-    {{ home.pricePerNight }} / night <br/>
+  <div v-for="home in homes" :key="home.objectID" style="float:left; margin:10px ">
+    <home-card :home="home"/>
   </div>
   </div>
 </template>
 
 <script>
 import homes from '~/data/homes'
-
+import HomeCard from '~/components/HomeCard'
 export default {
+  components: {
+    HomeCard
+  },
   data(){
     return {
       homes: homes.slice(0,3)
