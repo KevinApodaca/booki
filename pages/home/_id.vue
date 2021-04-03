aae46363453efcd46e665a547398c4640dc75b9d
Kevin Apodaca <kevin.apodaca7@gmail.com>
2021-04-03 17:49:45 -0600

    :lipstick: show the host data

--- pages/home/_id.vue
+++ pages/home/_id.vue
@@ -18,6 +18,12 @@
       <short-text :text="review.comment" :target="150"/><br/>
     </div>
 
+    <img :src="user.image"/> <br/>
+    {{ user.name }} <br/>
+    {{ formatDate(user.joined) }} <br/>
+    {{ user.reviewCount }} <br/>
+    {{ user.description }} <br/>
+
   </div>
 </template>
 
@@ -38,9 +44,13 @@ export default {
     const reviewResponse = await $dataApi.getReviewsByHomeId(params.id)
     if(!reviewResponse.ok) return error({ statusCode: reviewResponse.status, message: reviewResponse.statusText})
 
+    const userResponse = await $dataApi.getUserByHomeId(params.id)
+    if(!userResponse.ok) return error({ statusCode: userResponse.status, message: userResponse.statusText})
+
     return {
       home: homeResponse.json,
       reviews: reviewResponse.json.hits,
+      user: userResponse.json.hits[0]
     }
   },
   methods: {
