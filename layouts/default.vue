d57ac1525cf852fecbb614967a2f856ca4583e3c
Kevin Apodaca <kevin.apodaca7@gmail.com>
2021-04-03 21:23:31 -0600

    passing proper geometry

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,7 +2,7 @@
   <div>
     <header style="background-color:#eee;">
       <nuxt-link to="/">Home</nuxt-link>
-      <input type="text" ref="citySearch"/>
+      <input type="text" ref="citySearch" @chaged="changed"/>
     </header>
     <nuxt/>
   </div>
@@ -11,6 +11,21 @@
 export default {
   mounted() {
     this.$maps.makeAutoComplete(this.$refs.citySearch)
+  },
+  methods: {
+    changed(event) {
+      const place = event.detail
+      if(!place.geometry) return
+
+      this.$router.push({
+        name: 'search',
+        query: {
+          lat: place.geometry.location.lat(),
+          lng: place.geometry.location.lng(),
+          label: this.$refs.citySearch.value,
+        }
+      })
+    }
   }
 }
 </script>
