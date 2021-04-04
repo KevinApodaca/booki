41c3397a15ffbeed96a389d5eb853a9fdad3ffc2
Kevin Apodaca <kevin.apodaca7@gmail.com>
2021-04-04 09:51:12 -0600

    :lipstick: toggle highlight colors on hover

--- pages/search.vue
+++ pages/search.vue
@@ -4,7 +4,7 @@
     <div style="height:800px; width:800px; float:right;" ref="map"></div>
     <div v-if="homes.length > 0">
       <nuxt-link v-for="home in homes" :key="home.objectID" :to="`/home/${home.objectID}`">
-        <HomeRow :home="home"/>
+        <HomeRow :home="home" @mouseover.native="highlightMarker(home.objectID, true)" @mouseout.native="highlightMarker(home.objectID, false)"/>
       </nuxt-link>
     </div>
     <div v-else> No Results Found </div>
@@ -21,6 +21,9 @@ export default {
     this.updateMap()
   },
   methods: {
+    highlightMarker(homeId, isHighlighted) {
+      document.getElementsByClassName(`home-${homeId}`)[0]?.classList?.toggle('marker-highlight', isHighlighted)
+    },
     updateMap() {
        this.$maps.showMap(this.$refs.map, this.lat, this.lng, this.getHomeMarkers())
     },
@@ -29,6 +32,7 @@ export default {
         return {
           ...home._geoloc,
           pricePerNight: home.pricePerNight,
+          id: home.objectID,
         }
       })
     }
@@ -60,5 +64,11 @@ export default {
   font-weight: bold;
   border-radius: 20px;
   padding: 5px 8px;
+}
+
+.marker-highlight {
+  color: white !important;
+  background-color: black;
+  border-color: black;
 }
 </style>
