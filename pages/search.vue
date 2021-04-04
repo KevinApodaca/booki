b3d4aa20def114e1fa086bf9a5b795ce52783ad5
Kevin Apodaca <kevin.apodaca7@gmail.com>
2021-04-03 22:06:09 -0600

    :goal_net: handle no results found

--- pages/search.vue
+++ pages/search.vue
@@ -1,11 +1,19 @@
 <template>
   <div>
     {{ lat }} / {{ lng }} / {{ label }}<br/>
-    <HomeRow v-for="home in homes" :key="home.objectID" :home="home"/>
+    <div v-if="homes.length > 0">
+      <HomeRow v-for="home in homes" :key="home.objectID" :home="home"/>
+    </div>
+    <div v-else> No Results Found </div>
   </div>
 </template>
 <script>
 export default {
+  head() {
+    return {
+      title: `Homes around ${this.label}`
+    }
+  },
   async beforeRouteUpdate(to, from, next) {
     const data = await this.$dataApi.getHomesByLocation(to.query.lat, to.query.lng)
     this.homes = data.json.hits
