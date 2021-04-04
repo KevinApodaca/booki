cbe43b35ede0b412d8d12da0054efd7198eecb23
Kevin Apodaca <kevin.apodaca7@gmail.com>
2021-04-03 21:40:34 -0600

    :sparkles: geosearch added

--- plugins/dataApi.js
+++ plugins/dataApi.js
@@ -10,7 +10,9 @@ export default function(context,inject) {
     getHome,
     getReviewsByHomeId,
     getUserByHomeId,
+    getHomesByLocation,
   })
+
   async function getHome(homeId) {
     try {
       return unWrap(await fetch(`https://${appId}-dsn.algolia.net/1/indexes/homes/${homeId}`, { headers }))
@@ -42,6 +44,23 @@ export default function(context,inject) {
         method: 'POST',
         body: JSON.stringify({
           filters: `homeId:${homeId}`,
+          attributesToHighlight: []
+        })
+      }))
+    } catch(error) {
+      return getErrorResponse(error)
+    }
+  }
+
+  async function getHomesByLocation(lat,lng,radiusInMeters=1500) {
+    try {
+      return unWrap(await fetch(`https://${appId}-dsn.algolia.net/1/indexes/homes/query`, {
+        headers,
+        method: 'POST',
+        body: JSON.stringify({
+          aroundLatLng: `${lat},${lng}`,
+          aroundRadius: radiusInMeters,
+          hitsPerPage: 10,
           attributesToHighlight: []
         })
       }))
