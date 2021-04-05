333ce2a48eb7d6c061da612b9fae3c9612f6b81a
Kevin Apodaca <kevin.apodaca7@gmail.com>
2021-04-05 11:11:57 -0600

    :egg: component to show host of property

---
+++ components/PropertyHost.vue
@@ -0,0 +1,24 @@
+<template>
+<div class="app-background-grey">
+  <div class="app-wrapper app-padded-vertical">
+    <div class="app-host-header">
+      <div><img :src="user.image"/></div>
+      <div>
+        <div class="app-host-name">{{ user.name }}</div>
+      </div>
+    </div>
+    <div class="app-host-description"></div>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+  props: {
+    user: {
+      type: Object,
+      required: true,
+    }
+  }
+}
+</script>
