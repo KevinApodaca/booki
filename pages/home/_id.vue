95f431021c71a57bf1e6f751f51f7fdae7ed0a4c
Kevin Apodaca <kevin.apodaca7@gmail.com>
2021-04-05 11:21:31 -0600

    :fire: remove old code

--- pages/home/_id.vue
+++ pages/home/_id.vue
@@ -6,44 +6,15 @@
     <PropertyMap :home="home"/>
     <PropertyReviews :reviews="reviews"/>
     <PropertyHost :user="user"/>
-
-    <div style="display:flex;">
-      <img v-for="image in home.images" :key="image" alt="Listing Image" :src="image" width="200" height="150"/>
-    </div>
-    {{ home.title }}<br/>
-    ${{ home.pricePerNight }} / night <br/>
-    <img src="/images/marker.svg" width="20" height="20"/>{{ home.location.address }} {{ home.location.city }}, {{ home.location.state }}, {{ home.location.country }}<br/>
-    <img src="/images/star.svg" width="20" height="20"/>{{ home.reviewValue }}<br/>
-    {{ home.guests }} guests, {{ home.bedrooms }} rooms, {{ home.beds }} beds, {{ home.bathrooms }}, bathrooms <br/>
-    {{ home.description }}
-    <div style="height:800px; width:800px" ref="map"></div>
-
-    <div v-for="review in reviews" :key="review.objectID">
-      <img :src="review.reviewer.image"/><br/>
-      {{ review.reviewer.name }}<br/>
-      {{ formatDate(review.date) }}<br/>
-      <short-text :text="review.comment" :target="150"/><br/>
-    </div>
-
-    <img :src="user.image"/> <br/>
-    {{ user.name }} <br/>
-    {{ formatDate(user.joined) }} <br/>
-    {{ user.reviewCount }} <br/>
-    {{ user.description }} <br/>
-
   </div>
 </template>
 
 <script>
-import formatDate from '~/utils/formatDate'
 export default {
   head() {
     return {
       title: this.home.title,
     }
-  },
-  mounted() {
-    this.$maps.showMap(this.$refs.map, this.home._geoloc.lat, this.home._geoloc.lng)
   },
   async asyncData({ params, $dataApi, error }) {
     const responses = await Promise.all([
@@ -60,9 +31,6 @@ export default {
       reviews: responses[1].json.hits,
       user: responses[2].json.hits[0]
     }
-  },
-  methods: {
-    formatDate
   }
 }
 </script>
