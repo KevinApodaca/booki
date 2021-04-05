00d923cfc20c34c61a82a16ffffbdd0d3430cebe
Kevin Apodaca <kevin.apodaca7@gmail.com>
2021-04-05 10:57:41 -0600

    :egg: create component to show reviews

---
+++ components/PropertyReviews.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="app-padded-vertical">
+    <div class="app-wrapper">
+      <div class="app-testimonials">
+        <div v-for="(review, index) in reviews" :key="index">
+          <div class="app-testimonial-header">
+            <div>
+              <img :src="review.reviewer.image"/>
+            </div>
+            <div>
+              <p class="app-testimonial-name">
+                {{ review.reviewer.name }}
+              </p>
+            </div>
+          </div>
+          <div class="app-testimonial-body"></div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    reviews: {
+      type: Array,
+      required: true,
+    }
+  }
+}
+</script>
